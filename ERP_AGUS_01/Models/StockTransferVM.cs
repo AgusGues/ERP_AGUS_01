7cc4f8b1dc363c74c88dd5c62abfe26424e9bc08
Agus <agusgus612@gmail.com>
2026-01-30 22:58:06 +0700

    add

---
+++ ERP_AGUS_01/Models/StockTransferVM.cs
@@ -0,0 +1,14 @@
+﻿namespace ERP_AGUS_01.Models
+{
+    public class StockTransferVM
+    {
+        public int TransferId { get; set; }
+        public string TransferNumber { get; set; }
+        public DateTime TransferDate { get; set; }
+        public int FromWarehouseId { get; set; }
+        public int FromLocationId { get; set; }
+        public int ToWarehouseId { get; set; }
+        public int ToLocationId { get; set; }
+        public string Status { get; set; }
+    }
+}
